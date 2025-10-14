d40967d1897502defb1d4697bffecbe811348b39
Suhail Suhail <suhailece@gmail.com>
2025-10-14 15:19:18 -0700

    Add pretty print to struct

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -21,11 +21,12 @@ pub fn area_ref(r: &Rect) -> u32 { r.w * r.h }
 
 pub fn demo() {
     let mut r = Rect { w: 30, h: 50 };
-    println!("area (method): {}", r.area());
+    println!("Area:\n (method): {}", r.area());
     println!("area (free): {}", area_ref(&r));
     println!("rect (debug): {:?}", r);
     r.make_square(20);
     println!("made square: {:?}, area={}", r, r.area());
+    println!("Pretty:\n{:#?}", r);
 }
 
 #[cfg(test)]
