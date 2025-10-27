1beb448b8591ade222f5cd3a55d455cad1ded769
Suhail Suhail <suhailece@gmail.com>
2025-10-27 11:11:11 -0700

    Trying out how to read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use rand::Rng;
+
 fn main(){
-    println!("Hello, world");
+  let mut guess = String::new();
+  let sec_num = rand::rng().random_range(1..=100);
+
+  println!("Pick a number from 1 to 100");
+  println!("What's your pick:");
+
+  io::stdin()
+    .read_line(&mut guess)
+    .expect("You didn't do what I asked you to do, try again");
+
+  println!("You picked: {guess}");
+  println!("And I picked {sec_num}");
+
 }
