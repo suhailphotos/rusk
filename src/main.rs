81bc2ab6165ef9e481162a2bfb2ddbab35a4c4e5
Suhail Suhail <suhailece@gmail.com>
2025-10-31 13:04:06 -0700

    Understanding owership and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-use std::io;
-
 fn main() {
-  println!("Let's play a guessing game.\nGuess a number:");
+  let mut s = String::from("Hello");
+  s.push_str(", World");
 
-  let mut guess = String::new();
-
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read the input");
-
-  let guess: u32 = match guess.trim().parse() {
-    Ok(num) => num,
-    Err(e) => {
-      println!("Please enter a valid number. Error: {e}");
-      return;
-    },
-  };
-
-  println!("You guessed {guess}");
+  println!("{s}");
 }
