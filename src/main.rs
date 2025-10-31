d1804cd56730b6819568a43ce7b5fe26cabb3534
Suhail Suhail <suhailece@gmail.com>
2025-10-31 12:49:46 -0700

    Practicing guessing game

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ use std::io;
 
 fn main() {
   println!("Let's play a guessing game.\nGuess a number:");
+
   let mut guess = String::new();
 
   io::stdin()
     .read_line(&mut guess)
-    .expect("Failed to read the input!");
+    .expect("Failed to read the input");
 
   let guess: u32 = match guess.trim().parse() {
     Ok(num) => num,
     Err(e) => {
-      println!("I ran into this error {e}");
+      println!("Please enter a valid number. Error: {e}");
       return;
     },
   };
-  println!("The number you gussed is {guess}")
+
+  println!("You guessed {guess}");
 }
