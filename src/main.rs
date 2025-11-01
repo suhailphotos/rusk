d77252a7fa12aa8d2b3e4fa50107b1e5e952fd9c
Suhail Suhail <suhailece@gmail.com>
2025-11-01 09:37:58 -0700

    Implement my own debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-fn main() {
-  let mut s = String::from("Hello");
-  s.push_str(", World");
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
 
-  println!("{s}");
+impl std::fmt::Debug for Rectangle {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "Rectangle {{ Widht: {}, Height: {} }}", self.width, self.height)
+  }
+}
+
+fn main() {
+  let rect1 = Rectangle {
+    width: 32,
+    height: 83,
+  };
+
+  println!("{rect1:?}");
 }
