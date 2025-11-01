8487e3cb9efa547b7e65df644f45c15d16c373e5
Suhail Suhail <suhailece@gmail.com>
2025-11-01 12:34:32 -0700

    Debug trait implementation

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ struct Rectangle {
 
 impl std::fmt::Debug for Rectangle {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "Rectangle {{ Widht: {}, Height: {} }}", self.width, self.height)
+    write!(f, "Its a rectangle with width: {} & height: {}", self.width, self.height)
   }
 }
 
 fn main() {
   let rect1 = Rectangle {
     width: 32,
-    height: 83,
+    height: 93,
   };
 
   println!("{rect1:?}");
